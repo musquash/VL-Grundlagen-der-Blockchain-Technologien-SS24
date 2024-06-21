304f74ff9d2c048e27be926cc165e201ff1b1c51
musquash-mac <musquash@gmx.net>
2024-06-21 10:30:53 +0200

    ADD :: blockchain class and exercise support

---
+++ Ethereum/06-exercise.py
@@ -0,0 +1,42 @@
+"""
+Hilfestellung zu den Aufgaben im Übungsblatt 6 der Vorlesung Grundlagen der Blockchain-Technologien im Sommersemester 24
+"""
+
+from Ethereum.gbt.blockchain import GBT, send_tx
+
+# First you have to init an instance. You can connect to your own node by put in the RPC information:
+# GBT('http://<url.com|IP>:<port>)
+gbt = GBT()
+
+# You can check, if the connection to the node is possible
+gbt.w1.is_connected()
+print(f"Node 1 ist verbunden: {gbt.w1.is_connected()} \n"
+      f"Node 2 ist verbunden: {gbt.w2.is_connected()} \n"
+      f"Node 3 ist verbunden: {gbt.w3.is_connected()}")
+
+# For sending a transaction you first need an account. You can import it from a private key or just create a fresh new
+# account. Make sure to save the private key to be able to retore the account if you restart the program. Otherwise a
+# new accoutnt with again no funds will be created:
+account = gbt.w1.eth.account.create("exercise")
+account_from_key = gbt.w1.eth.account.from_key(account._private_key)
+
+print(f"You have an account with address {account.address} \n"
+      f"and prviate key {account._private_key}")
+print(f"You have an imported account with address {account_from_key.address} \n"
+      f"and prviate key {account_from_key._private_key}")
+
+
+# Now the transaction must be initialized and sent from an account with a balance.
+tx_hash = send_tx(sender=account._private_key, receiver=account.address, web3=gbt.w1, amount=10000)
+
+
+print(f"Transaction with hash {tx_hash} was successfully deployed.")
+
+
+
+# Read data from a transaction
+
+
+
+
+
---
+++ Ethereum/gbt/blockchain.py
@@ -0,0 +1,42 @@
+from web3 import Web3
+
+
+def send_tx(sender, receiver, amount, web3):
+    """
+    Method for sending a transaction in (the private) ETH-Blockchain.
+
+    :param sender:  private key from sending account
+    :param receiver: hash value of the pub-key of the recipient as a string
+                    (i.e: '0xf54DFE141A3a02BD6a0a85d50360B044449C9aB5')
+    :param amount: amount of wei you want to send
+    :param web3:  Instance of web3 connection to a node. Defines to which blockchain is connected and which methods
+                  are available.
+    :return:
+    """
+    account = web3.eth.account.from_key(sender)
+
+    # create tx
+    signed_txn = web3.eth.account.sign_transaction(dict(
+        nonce=web3.eth.get_transaction_count(account.address),
+        chainId=108,
+        gasPrice=web3.eth.gas_price,
+        gas=100000,
+        to=receiver,
+        value=amount,
+        data=b'',
+    ),
+        account._private_key,
+    )
+
+    tx_hash = web3.eth.send_raw_transaction(signed_txn.rawTransaction)
+    return tx_hash
+
+
+class GBT:
+
+    def __init__(self, rpc_url=''):
+        self.w1 = Web3(Web3.HTTPProvider('http://62.171.152.173:9001'))
+        self.w2 = Web3(Web3.HTTPProvider('http://144.91.79.46:9001'))
+        self.w3 = Web3(Web3.HTTPProvider('http://144.91.79.79:9001'))
+        if rpc_url != '':
+            self.w4 = Web3(Web3.HTTPProvider(rpc_url))
